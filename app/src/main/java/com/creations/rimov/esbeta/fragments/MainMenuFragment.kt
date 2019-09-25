f6670da02d553a3394876f8b1bdb58f6761ca7e2
Rinat Ibragimov <ruskirin@gmail.com>
2019-09-25 08:37:43 -0400

    Bare interface, addressing navigation to Video

--- app/src/main/java/com/creations/rimov/esbeta/fragments/MainMenuFragment.kt
+++ app/src/main/java/com/creations/rimov/esbeta/fragments/MainMenuFragment.kt
@@ -1,25 +1,36 @@
 package com.creations.rimov.esbeta.fragments
 
+import android.content.Context
 import android.os.Bundle
+import android.util.AttributeSet
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageButton
+import android.widget.RelativeLayout
+import android.widget.TextView
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.creations.rimov.esbeta.R
-import kotlinx.android.synthetic.main.main_menu.view.*
+import com.creations.rimov.esbeta.extensions.gone
+import com.creations.rimov.esbeta.extensions.visible
+import kotlinx.android.synthetic.main.menu_button_expandable.view.*
+import kotlinx.android.synthetic.main.menu_main.view.*
 
 class MainMenuFragment : Fragment(), View.OnClickListener {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
 
-        val view = inflater.inflate(R.layout.main_menu, container, false)
+        val view = inflater.inflate(R.layout.menu_main, container, false)
 
-        view.menuA.setGoListener(this)
-        view.menuB.setGoListener(this)
-        view.menuC.setGoListener(this)
-        view.menuD.setGoListener(this)
+        (view.menuA as MenuBtnExpand).setGoListener(this)
+        (view.menuB as MenuBtnExpand).setGoListener(this)
+        (view.menuC as MenuBtnExpand).setGoListener(this)
+        (view.menuD as MenuBtnExpand).setGoListener(this)
 
         return view
     }
@@ -27,8 +38,12 @@ class MainMenuFragment : Fragment(), View.OnClickListener {
     override fun onClick(v: View?) {
 
         v?.let {
+            Log.i("MainMenuFrag", "onClick(): is click on btnGo? ${it.id == R.id.menuBtnGo}")
+
             when(it.id) {
                 R.id.menuA -> {
+                    Log.i("MainMenuFrag", "onClick(): click to next fragment")
+
                     findNavController().navigate(R.id.action_mainMenuFragment_to_videoFragment)
                 }
                 R.id.menuB -> {
@@ -40,6 +55,50 @@ class MainMenuFragment : Fragment(), View.OnClickListener {
                 R.id.menuD -> {
 
                 }
+            }
+        }
+    }
+
+    class MenuBtnExpand(context: Context, attrs: AttributeSet?)
+        : RelativeLayout(context, attrs) {
+
+        private val btnExpand: Button by lazy {menuBtnExpand}
+        private val textDetail: TextView by lazy {menuDetailText}
+        private val btnGo: ImageButton by lazy {menuBtnGo}
+
+        init {
+            View.inflate(context, R.layout.menu_button_expandable, this)
+
+            attrs?.let { set ->
+                context.obtainStyledAttributes(set, R.styleable.MenuBtnExpand)
+                    .let {
+                        btnExpand.text = it.getString(
+                            R.styleable.MenuBtnExpand_button_text) ?: "def name"
+                        textDetail.text = it.getString(
+                            R.styleable.MenuBtnExpand_detail_text) ?: "def text"
+
+                        it.recycle()
+                    }
+            }
+
+            btnExpand.setOnClickListener {
+                toggleDetail()
+            }
+        }
+
+        fun setGoListener(listener: OnClickListener) {
+            btnGo.setOnClickListener(listener)
+        }
+
+        private fun toggleDetail() {
+
+            if(textDetail.isVisible) {
+                textDetail.gone()
+                btnGo.gone()
+
+            } else {
+                textDetail.visible()
+                btnGo.visible()
             }
         }
     }
--- app/src/main/java/com/creations/rimov/esbeta/fragments/VideoFragment.kt
+++ app/src/main/java/com/creations/rimov/esbeta/fragments/VideoFragment.kt
@@ -12,7 +12,7 @@ class VideoFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
 
-        val view = inflater.inflate(R.layout.video_test, container, false)
+        val view = inflater.inflate(R.layout.testing_video, container, false)
 
         return view
     }
