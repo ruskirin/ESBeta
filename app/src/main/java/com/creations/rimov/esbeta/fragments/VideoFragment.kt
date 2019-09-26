4d7009ff2b859240a50c7fd405dfac2645ceb4c0
Rinat Ibragimov <ruskirin@gmail.com>
2019-09-26 00:09:13 -0400

    Work on synchronizing recording with video playback

--- app/src/main/java/com/creations/rimov/esbeta/fragments/VideoFragment.kt
+++ app/src/main/java/com/creations/rimov/esbeta/fragments/VideoFragment.kt
@@ -1,5 +1,6 @@
 package com.creations.rimov.esbeta.fragments
 
+import android.media.CamcorderProfile
 import android.media.MediaRecorder
 import android.net.Uri
 import android.os.Bundle
@@ -12,11 +13,19 @@ import androidx.fragment.app.Fragment
 import com.creations.rimov.esbeta.R
 import kotlinx.android.synthetic.main.testing_video.*
 import kotlinx.android.synthetic.main.testing_video.view.*
+import java.io.File
+import java.io.IOException
+import java.lang.Exception
+import java.text.SimpleDateFormat
+import java.util.*
 
 class VideoFragment : Fragment() {
 
     private lateinit var vidView: VideoView
     private lateinit var vidUri: Uri
+
+    private val btnStart by lazy {videoBtnStart}
+    private val btnStop by lazy {videoBtnStop}
 
     private val recorder by lazy {MediaRecorder()}
 
@@ -33,18 +42,12 @@ class VideoFragment : Fragment() {
 
     private fun startVideo() {
 
+        if(vidView.isPlaying) return
+
         vidUri = Uri.parse("android.resource://com.creations.rimov.esbeta/" + R.raw.sample_video)
 
         vidView.setVideoURI(vidUri)
         vidView.start()
-    }
-
-    private fun resumeVideo() {
-        if(!vidView.isPlaying) vidView.resume()
-    }
-
-    private fun pauseVideo() {
-        if(vidView.canPause()) vidView.pause()
     }
 
     private fun stopVideo() {
@@ -53,11 +56,32 @@ class VideoFragment : Fragment() {
 
     private fun initRecorder() {
 
-        recorder.setVideoSource(MediaRecorder.VideoSource.CAMERA)
+        val vid = File(context?.filesDir,
+            SimpleDateFormat("MM/dd_HHmmss", Locale.US)".mp4")
+
+        recorder.apply {
+            setVideoSource(MediaRecorder.VideoSource.CAMERA)
+//            setProfile(CamcorderProfile.get(CamcorderProfile.QUALITY_HIGH))
+            setOutputFormat(MediaRecorder.OutputFormat.MPEG_4)
+            setVideoEncoder(MediaRecorder.VideoEncoder.H264)
+//            setVideoSize()
+            setOutputFile(vid.absolutePath)
+            Log.i("VideoFrag", "initRecorder(): saving in path ${vid.absolutePath}")
+        }
     }
 
-    private fun getFrontCameraId(): Int {
+    private fun prepareRecorder() {
 
-        //android.hardware.camera2. ---- use the MediaRecorder but first you need to let it know to use the front camera
+        try {
+            recorder.prepare()
+
+        } catch(e: IOException) {
+            e.printStackTrace()
+        }
     }
+
+//    private fun getFrontCameraId(): Int {
+//
+//        //android.hardware.camera2. ---- use the MediaRecorder but first you need to let it know to use the front camera
+//    }
 }
